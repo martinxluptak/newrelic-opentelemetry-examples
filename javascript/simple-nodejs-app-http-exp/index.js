8be2afb07556e2cb4fde09ecd18896deb59e9e5e
Martin Luptak <martin.luptak@apify.com>
2022-09-07 10:53:40 +0200

    Add opentelemetry-js metrics to demonstrate its not being sent

--- javascript/simple-nodejs-app-http-exp/index.js
+++ javascript/simple-nodejs-app-http-exp/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const api = require("@opentelemetry/api");
+const { OTLPMetricExporter } = require('@opentelemetry/exporter-metrics-otlp-proto');
+const { Meter, MeterProvider, PeriodicExportingMetricReader } = require('@opentelemetry/sdk-metrics-base');
+const { Resource } = require('@opentelemetry/resources');
+const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
 
 
 const PORT = process.env.PORT || "8080";
@@ -16,9 +20,32 @@ function getLargeValue() {
 
 largeValue = getLargeValue()
 
+
+
+const exporter = new OTLPMetricExporter({
+	// compression: CompressionAlgorithm.GZIP,
+	// credentials: credentials.createSsl(),
+});
+
+const resource = new Resource({
+  [SemanticResourceAttributes.SERVICE_NAME]: "OpenTelemetry-Node.JS-Example",
+});
+
+const meterProvider = new MeterProvider(resource);
+            meterProvider.addMetricReader(new PeriodicExportingMetricReader({
+                exporter,
+                exportIntervalMillis: 2000,
+            }));
+
+const meter = meterProvider.getMeter('myMeter');
+const counter = meter.createCounter('test.request.counter');
+
+
 app.get("/ping", (_, res) => {
   const currentSpan = api.trace.getSpan(api.context.active());
-
+  counter.add(1, {
+	env: 'dev'
+  });
   // New Relic only accepts attributes values that are less than 4096 characters.
   // When viewing this span in New Relic, the value of the "truncate" attribute will contain no Bs
   currentSpan.setAttribute("truncate", largeValue)
